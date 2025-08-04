67ddc6ecf17e9084f078042902edc2e7e469baa6
Nico Culotta <nicoculotta@MacBook-Pro-de-admin.local>
2025-08-04 19:18:44 +0200

    fix tracker with suspence

--- components/GoogleTagManager/PageViewTracker.tsx
+++ components/GoogleTagManager/PageViewTracker.tsx
@@ -1,19 +1,25 @@
 "use client";
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { gtmPageView } from '@/utils/gtm';
 
-export default function PageViewTracker() {
+function PageViewTrackingLogic() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    // Construir la URL completa
     const url = `${pathname}${searchParams.toString() ? `?${searchParams.toString()}` : ''}`;
     
-    // Enviar el evento de page view a GTM
     gtmPageView(url);
   }, [pathname, searchParams]);
 
-  return null; // Este componente no renderiza nada visual
+  return null;
+}
+
+export default function PageViewTracker() {
+  return (
+    <Suspense fallback={null}>
+      <PageViewTrackingLogic />
+    </Suspense>
+  );
 }
